9151db7e6cc13429ce5899276ded4e8440db359e
AngeNicole <angenicolemukundwa22@gmail.com>
2024-09-14 23:24:45 +0400

    change

--- models/__init__.py
+++ models/__init__.py
@@ -17,7 +17,7 @@ loaded_objects = storage.all()
 for key, value in loaded_objects.items():
     if key in storage.models:
         continue
-    class_name = value["__class__"]
+    class_name = value("__class__")
     del value["__class__"]
 classes = {
     "BaseModel": BaseModel,
