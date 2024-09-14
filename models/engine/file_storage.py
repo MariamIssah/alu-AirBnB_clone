7c2b35f8b5629a750d7da121971f00e2a1c471a9
AngeNicole <angenicolemukundwa22@gmail.com>
2024-09-14 22:20:00 +0400

    error correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python3
-"""
-This module defines a FileStorage class that stores
-and retrieves objects to and from a JSON file.
-"""
-
 import json
+from json.decoder import JSONDecodeError
+from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,60 +8,130 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from .errors import ModelNotFoundError, InstanceNotFoundError
 
 
 class FileStorage:
-    """
-    A file storage system for storing and retrieving objects.
-    """
+    """This class serves as an ORM to interface between our Storage System"""
 
-    __file_path = "file.json"
     __objects = {}
+    __file_path = "file.json"
+    models = (
+        "BaseModel",
+        "User", "City", "State", "Place",
+        "Amenity", "Review"
+    )
+
+    def _init_(self):
+        """constructor"""
+        pass
 
     def all(self):
-        """
-        Returns the dictionary of all objects currently stored.
-
-        Returns:
-            dict: A dictionary of all objects currently stored.
-        """
+        """Return all instances stored"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Adds a new object to the storage.
-
-        Args:
-            obj (object): The object to be added to the storage.
-        """
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        """Stores a new Object"""
+        key = "{}.{}".format(type(obj)._name_, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        Saves the objects in the storage to a JSON file.
-        """
-        obj_dict = {}
-
-        for key, value in self.__objects.items():
-            obj_dict[key] = value.to_dict()
-
-        try:
-            with open(self.__file_path, 'w') as file:
-                json.dump(obj_dict, file, indent=2)
-        except FileNotFoundError:
-            pass
+        """serializes objects stored and persist in file"""
+        serialized = {
+            key: val.to_dict()
+            for key, val in self.__objects.items()
+        }
+        with open(FileStorage.__file_path, "w") as f:
+            f.write(json.dumps(serialized))
 
     def reload(self):
-        """
-        Reloads the objects from the JSON file into the storage.
-        """
+        """de-serialize persisted objects"""
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                obj_dict = json.load(file)
-
-                for key, value in obj_dict.items():
-                    self.__objects[key] = eval(
-                        f"{value['__class__']}(**{value})")
-
-        except FileNotFoundError:
+            deserialized = {}
+            with open(FileStorage.__file_path, "r") as f:
+                deserialized = json.loads(f.read())
+            FileStorage.__objects = {
+                key:
+                    eval(obj["_class_"])(**obj)
+                for key, obj in deserialized.items()}
+        except (FileNotFoundError, JSONDecodeError):
+            # No need for error
             pass
+
+    def find_by_id(self, model, obj_id):
+        """Find and return an elemt of model by its id"""
+        F = FileStorage
+        if model not in F.models:
+            # Invalid Model Name
+            # Not yet Implemented
+            raise ModelNotFoundError(model)
+
+        key = model + "." + obj_id
+        if key not in F.__objects:
+            # invalid id
+            # Not yet Implemented
+            raise InstanceNotFoundError(obj_id, model)
+
+        return F.__objects[key]
+
+    def delete_by_id(self, model, obj_id):
+        """Find and return an elemt of model by its id"""
+        F = FileStorage
+        if model not in F.models:
+            raise ModelNotFoundError(model)
+
+        key = model + "." + obj_id
+        if key not in F.__objects:
+            raise InstanceNotFoundError(obj_id, model)
+
+        del F.__objects[key]
+        self.save()
+
+    def find_all(self, model=""):
+        """Find all instances or instances of model"""
+        if model and model not in FileStorage.models:
+            raise ModelNotFoundError(model)
+        results = []
+        for key, val in FileStorage.__objects.items():
+            if key.startswith(model):
+                results.append(str(val))
+        return results
+
+    def update_one(self, model, iid, field, value):
+        """Updates an instance"""
+        F = FileStorage
+        if model not in F.models:
+            raise ModelNotFoundError(model)
+
+        key = model + "." + iid
+        if key not in F.__objects:
+            raise InstanceNotFoundError(iid, model)
+        if field in ("id", "updated_at", "created_at"):
+            # not allowed to be updated
+            return
+        inst = F.__objects[key]
+        try:
+            # if instance has that value
+            # cast it to its type
+            vtype = type(inst._dict_[field])
+            inst._dict_[field] = vtype(value)
+        except KeyError:
+            # instance doesn't has the field
+            # assign the value with its type
+            inst._dict_[field] = value
+        finally:
+            inst.updated_at = datetime.utcnow()
+            self.save()
+
+    def reset(self):
+        """Resets the storage system to a clean state"""
+        # Clear all objects from memory
+        FileStorage.__objects.clear()
+
+        # Optionally, delete the storage file or clear its content
+        try:
+            with open(FileStorage.__file_path, "w") as f:
+                f.write("{}")  # Write an empty JSON object to the file
+        except IOError as e:
+            # Handle file errors, e.g., permission issues, file not found, etc.
+            print(f"Error resetting storage file: {e}")
